cf6e4d6eb520edf965e00968e55f183d6ec90a55
Jiwon Moon <mon99745@dreamsecurity.com>
2026-01-06 15:39:14 +0900

    ref: update JPA repositories for analysis domain

--- aia-app/src/main/java/com/aiinsightagent/app/repository/ActorRepository.java
+++ aia-app/src/main/java/com/aiinsightagent/app/repository/ActorRepository.java
@@ -4,7 +4,12 @@ import com.aiinsightagent.app.entity.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ActorRepository
 		extends JpaRepository<Actor, Long> {
+	boolean existsByActorKey(String actorKey);
+
+	Optional<Actor> findByActorKey(String actorKey);
 }
